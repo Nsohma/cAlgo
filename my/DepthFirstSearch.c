59a33ed560de0f6bde774530235ca604b77bb108
Nsohma <niibo.s.aa@m.titech.ac.jp>
2025-06-24 12:05:14 +0900

    upgrade DFS

---
+++ my/DepthFirstSearch.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 100
+
+typedef struct cell{
+    int node;
+    struct cell *next;
+} cell;
+
+void preorder(int root, cell **S);
+void inorder(int root, cell **S);
+void postorder(int root, cell **S);
+
+int main(void){
+    struct cell *S[N], *p, *q;
+    int n, j, parent, child, root;
+    FILE *file;
+    file=fopen("treedata", "r");
+    //input the V number
+    fscanf(file, "%d", &n);
+    printf("n = %d\n", n);
+    //initialize S
+    for(j=0; j<n; j++) S[j]=NULL;
+    fscanf(file, "%d", &parent);
+    while(parent >= 0){
+        fscanf(file, "%d", &child);
+        if(child >= 0){
+            p=(struct cell *)malloc(sizeof(struct cell));
+            S[parent]=q=p;
+        } 
+        while(child >= 0){
+            q->node=child;
+            q->next=NULL;
+            fscanf(file, "%d", &child);
+            if(child >= 0){
+                p=(struct cell *)malloc(sizeof(struct cell));
+                q->next=p;
+                q=p;
+            }
+        }
+        fscanf(file, "%d", &parent);
+    }
+    for(j=0; j<n; j++) {
+        printf("S[%d] = %p, ", j, S[j]);
+        q=S[j];
+        while(q != NULL) {
+            printf("node = %d, next = %p, ", q->node, q->next);
+            q=q->next;
+        }
+        printf("\n");
+    }
+    root=0;
+    printf("preorder = ");
+    preorder(root, S);
+    printf("\n");
+    printf("inorder = ");
+    inorder(root, S);
+    printf("\n");
+    printf("postorder = ");
+    postorder(root, S);
+    printf("\n");
+    return(0);
+}
+
+void preorder(int root, cell **S){
+    cell *q;
+    printf("%d ", root);
+    q = S[root];
+    while (q != NULL){
+        preorder(q->node, S);
+        q = q -> next;
+    }
+}
+
+void inorder(int root, cell **S){
+    cell *q;
+    q = S[root];
+    if (q != NULL){
+        inorder(q->node, S);
+        q = q -> next;
+    }
+    printf("%d ", root);
+    while (q != NULL){
+        inorder(q -> node, S);
+        q = q -> next;
+    }
+}
+
+void postorder(int root, cell **S){
+    cell *q;
+    q = S[root];
+    while (q != NULL){
+        postorder(q -> node, S);
+        q = q -> next;
+    }
+    printf("%d ", root);
+}
+
