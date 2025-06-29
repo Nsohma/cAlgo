1ae746faa5ee3b9f99cdc15a476662a2e76ba98b
Nsohma <niibo.s.aa@m.titech.ac.jp>
2025-06-30 01:22:36 +0900

    create makefile and binary tree

---
+++ my/binary_tree.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum yn {yes, no};
+
+typedef struct node {
+    int element;
+    struct node *left;
+    struct node *right;
+} node;
+
+node *insert(int x, node *p);
+enum yn member(int x, node *p);
+void inorder(node *p);
+void preorder(node *p);
+int min(node *p);
+node *delete(int x, node *p);
+node *off(node *p);
+
+int main(void){
+    struct node *init;
+    int i, x;
+    enum yn a;
+   
+    init=NULL;
+    for(i=0; i<10; i++) init=insert((8-3*i)*(2-i/2), init);
+    init=insert(30, init);
+    init=insert(25, init);
+    init=insert(31, init);
+    init=insert(37, init);
+    printf("init = %p\n", init);
+    printf("inorder\n");
+    if(init != NULL) inorder(init);
+    printf("preorder\n");
+    if(init != NULL) preorder(init);
+    x=31;
+    a=member(x, init);
+    if(a==yes) printf("Yes: x = %d\n", x);
+    else printf("No: x = %d\n", x); 
+    x=-2;
+    a=member(x, init);
+    if(a==yes) printf("Yes: x = %d\n", x);
+    else printf("No: x = %d\n", x); 
+    x=min(init);
+    printf("min = %d\n", x);
+    //x=16;
+    //init=delete(x, init);
+    //printf("deleted x = %d\n", x);
+    //printf("init = %p\n", init);
+    //if(init != NULL) printpre(init);
+    //x=32;
+    //init=delete(x, init);
+    //printf("deleted x = %d\n", x);
+    //printf("init = %p\n", init);
+    //if(init != NULL) printpre(init);
+    //x=37;
+    //init=delete(x, init);
+    //printf("deleted x = %d\n", x);
+    //printf("init = %p\n", init);
+    //if(init != NULL) printpre(init);
+    return 0;
+}
+
+enum yn member(int x, node *p){
+    if (p == NULL) return no;
+    if (x == p->element) return yes;
+    else if (x > p->element) return member(x, p->right);
+    else if (x < p->element) return member(x, p->left);
+}
+
+node *insert(int x, node *p){
+    node *now, *prev;
+    node *new = (node *)malloc(sizeof(node));
+    if (p == NULL){ //measure on null pointer
+        new -> element = x;
+        new -> left = new -> right = NULL;
+        return new;
+    }
+    now = prev = p;
+    while (now != NULL){ // search x while now -> leaf
+        if (x == now -> element) {free(new); return p;}
+        else if (x > now -> element) {
+            prev = now;
+            now = now -> right;
+        }
+        else if (x < now -> element) {
+            prev = now;
+            now = now -> left;
+        }
+    }
+    //create new node to x
+    new -> element = x;
+    new -> left = NULL; new ->right = NULL;
+    if (x > prev->element) prev -> right = new;
+    if (x < prev->element) prev -> left = new;
+    return p;
+}
+
+node *delete(int x, node *p){
+    node *now, *prev;
+    if (p == NULL) exit(1);
+
+    now = prev = p;
+    while (now != NULL){
+        if (x == now -> element) break;
+        else{
+            prev = now;
+            if (x > now->element) now = now->right;
+            else now = now->left;
+        }
+    }
+    p = off(x);
+    return p;
+}
+
+node *off(node *p){
+    if (p == NULL) return NULL;
+    
+}
+
+void inorder(node *p){
+    if (p == NULL) return;
+    inorder(p->left); 
+    printf("p = %p, element = %d, left = %p, right = %p\n", 
+        p, p->element, p->left, p->right); 
+    inorder(p->right);
+}
+
+void preorder(node *p){
+    if (p == NULL) return;
+    printf("p = %p, element = %d, left = %p, right = %p\n", 
+        p, p->element, p->left, p->right); 
+    preorder(p->left);
+    preorder(p->right);
+}
+
+int min(node *p){
+    node *now = p;
+    if (p == NULL) exit(1);
+    while (now -> left != NULL){
+        now = now -> left;
+    }
+    return now->element;
+}
+
+
+
